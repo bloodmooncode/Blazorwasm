7f70c32d23d3ba594aa735c071576c35b682d340
ZYP <2359598489@qq.com>
2023-12-12 13:50:12 +0800

    basic layout for news and about page

--- tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  // 将blazor 会使用到 tailwind 的所有页面
-  content: ["**/*.razor", "**/*.cshtml", "**/*.html"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
