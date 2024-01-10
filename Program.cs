418a4e2baabfef51455897cc131832d64f870659
ZYP <2359598489@qq.com>
2024-01-10 18:30:01 +0800

    adjust-layout

--- Program.cs
+++ Program.cs
@@ -1,16 +1,13 @@
 using Blazorwasm;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Syncfusion.Blazor;
-
-//Register Syncfusion license
-Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("MzAwOTIzMEAzMjM0MmUzMDJlMzBRNjB5Z2NURXhGUlZEVzJYcWg5TXZucVRZam9wNXFPVzdJUm1TWGNzTUwwPQ==");
+using MudBlazor.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-
-builder.Services.AddSyncfusionBlazor();
+builder.Services.AddScoped<AppStateService>();
+builder.Services.AddMudServices();
 await builder.Build().RunAsync();
