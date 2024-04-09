30326272f63e6390f38ef61a4b553cb0db611212
ZYP <2359598489@qq.com>
2024-04-09 22:22:43 +0800

    add element with dialog

---
+++ Models/SellingPower.cs
@@ -0,0 +1,9 @@
+namespace Blazorwasm.Models
+{
+  public class SellingPower
+  {
+    public int Id { get; set; } = 0;
+    public string timeHorizon { get; set; } = "";
+    public double sellingpower { get; set; } = 0.0;
+  }
+}
